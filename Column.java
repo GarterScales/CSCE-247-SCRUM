14bb4884726f5b0a686098580b506c019c23feec
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-12 18:44:29 -0400

    working #18

---
+++ Column.java
@@ -0,0 +1,21 @@
+public class Column {
+    private ArrayList<Task> taskList;
+    private String columnName;
+
+    public Column(ArrayList<Task> tasks, String name) {
+        this.taskList = tasks;
+        this.columnName = name;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return this.taskList;
+    }
+
+    public void setName(String name) {
+        this.columnName = name;
+    }
+
+    public String getName() {
+        return columnName;
+    }
+}
