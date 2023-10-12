1837ee868503782be83e6bf9eeceb89f7cee8d9d
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-12 18:59:39 -0400

    working #16

---
+++ Gamification.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Gamification {
+    private int points;
+    private double bar;
+    private User user;
+
+    public int addPoints() {
+        return 0;
+    }
+
+    public String displayRank(ArrayList<User> users) {
+        return "";
+    }
+
+    public double displayBar() {
+        return bar;
+    }
+}
