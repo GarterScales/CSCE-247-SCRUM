549452c46efcd2106ee9b155d52800b2c5e2cf80
Sean Rinkleff <seanrinkleff@gmail.com>
2023-10-15 19:07:43 -0400

    Adding User class stub #7

---
+++ sean-stubs/User.java
@@ -0,0 +1,17 @@
+public class User 
+{
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+
+    public User(String firstName, String lastName, String email, String username, String password)
+    {
+        return User;
+    }
+    public boolean checkPasswordRequirements(String password)
+    {
+        return true;
+    }
+}
