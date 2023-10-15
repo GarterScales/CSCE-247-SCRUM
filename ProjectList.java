7db49d1e5d272ee88647a99124783e1cf04c325d
Cayden Scruggs <garterscales619@yahoo.com>
2023-10-15 19:21:48 -0400

    Project and ProjectList fix

--- ProjectList.java
+++ ProjectList.java
@@ -5,30 +5,31 @@ public class ProjectList {
     private User user;
 
     private ProjectList() {
-        
+
     }
 
     public static ProjectList getInstance() {
         return new ProjectList();
     }
 
-    public void ProjectBoard (User user, ArrayList<Project> projects) {
-        
+    public void ProjectBoard(User user, ArrayList<Project> projects) {
+
     }
 
     public ArrayList<Project> viewProjects(User user) {
         return new ArrayList<Project>();
     }
 
-    public Project addProject(ArrayList<Project> projects, String projectName) {
-        return new Project();
+    public void addProject(ArrayList<Project> projects, String projectName) {
+
     }
 
-    public Project removeProject(ArrayList<Project> projects, String projectName) {
-        return new Project();
+    public void removeProject(ArrayList<Project> projects, String projectName) {
+
     }
 
     public Project selectProject(ArrayList<Project> projects, String projectName) {
-        return new Project();
+        Project temp = new Project("temp");
+        return temp;
     }
 }
