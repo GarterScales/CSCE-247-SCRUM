e1067788a35dc04bf770c4df1ae550404cb5b18d
Cayden Scruggs <garterscales619@yahoo.com>
2023-10-16 17:59:38 -0400

    Fixed #64

---
+++ LibDataLoader.java
@@ -0,0 +1,16 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class LibDataLoader extends DataConstants{
+	
+	public static ArrayList<User> getUsers() {
+
+		
+		return null;
+	}
+}
