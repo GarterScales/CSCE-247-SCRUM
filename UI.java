7902f4e236de7433b0c12c9aa4c23cc18c2120c5
Chi Nguyen <chikn@email.sc.edu>
2023-10-18 17:31:53 -0400

    Working #65

--- UI.java
+++ UI.java
@@ -1,6 +1,53 @@
-public class UI {
-    public void run() {
+import java.util.Scanner;
 
+public class UI {
+    private SystemFACADE systemFacade;
+
+    public UI() {
+        systemFacade = new SystemFACADE();
+    }
+
+    public void run() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Would you like to: ");
+        System.out.println("1. Login");
+        System.out.println("2. Sign up");
+        int choice = keyboard.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Enter your username");
+                String username = keyboard.nextLine();
+                System.out.println("Enter your password");
+                String password = keyboard.nextLine();
+                User loggedIn = systemFacade.login(username, password);
+                if (loggedIn != null) {
+                    System.out.println("Login successfull");
+                } else {
+                    System.out.println("Login failed");
+                }
+                break;
+
+            case 2:
+                System.out.println("Enter your first name");
+                String firstName = keyboard.nextLine();
+                System.out.println("Enter your last name");
+                String lastName = keyboard.nextLine();
+                System.out.println("Enter your email");
+                String email = keyboard.nextLine();
+                System.out.println("Enter your username");
+                String newUsername = keyboard.nextLine();
+                System.out.println("Enter your password");
+                String newPassword = keyboard.nextLine();
+                User newUser = systemFacade.signUp(firstName, lastName, email, newUsername, newPassword);
+                if (newUser != null) {
+                    System.out.println("Sucessfully registered");
+                } else {
+                    System.out.println("Registration failed");
+                }
+                break;
+        }
+        keyboard.close();
     }
 
     public void scenario() {
@@ -8,6 +55,7 @@ public class UI {
     }
 
     public static void main(String[] args) {
-
+        UI ui = new UI();
+        ui.run();
     }
 }
