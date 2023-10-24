7898e5f7df230430f16fffc373b35ddc5c45be6a
Chi Nguyen <chikn@email.sc.edu>
2023-10-24 14:27:57 -0400

    adding run2

--- UI.java
+++ UI.java
@@ -38,48 +38,103 @@ public class UI {
                 String newUsername = keyboard.nextLine();
                 System.out.println("Enter your password");
                 String newPassword = keyboard.nextLine();
-                if (systemFacade.signUp(firstName, lastName, email, newUsername, newPassword)) {
-                    System.out.println("Sucessfully registered");
-                } else {
-                    System.out.println("Registration failed");
+                switch (systemFacade.signUp(firstName, lastName, email, newUsername, newPassword)) {
+                    case 0:
+                        System.out.println("Sucessfully registered");
+                        break;
+                    case 1:
+                        System.out.println(
+                                "Password insufficient, please use at least one upper case, one lower case, and one digit.");
+                        break;
+                    case 2:
+                        System.out.println("Username already exists, please choose a new Username.");
+                        break;
                 }
                 break;
         }
         keyboard.close();
     }
 
-    // only allows user to login
     public void run2() {
-        Scanner keyboard2 = new Scanner(System.in);
-        System.out.println("Enter your username");
-        String username = keyboard2.nextLine();
-        System.out.println("Enter your password");
-        String password = keyboard2.nextLine();
-        if (systemFacade.login(username, password)) {
-            System.out.println("Login successful");
-        } else {
-            System.out.println("Login failed");
-        }
-    }
-
-    // only allows user to sign up
-    public void run3() {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter your first name");
-        String firstName = keyboard.nextLine();
-        System.out.println("Enter your last name");
-        String lastName = keyboard.nextLine();
-        System.out.println("Enter your email");
-        String email = keyboard.nextLine();
-        System.out.println("Enter your username");
-        String newUsername = keyboard.nextLine();
-        System.out.println("Enter your password");
-        String newPassword = keyboard.nextLine();
-        if (systemFacade.signUp(firstName, lastName, email, newUsername, newPassword)) {
-            System.out.println("Sucessfully registered");
+        System.out.println("Would you like to make a project?");
+        System.out.println("1. Yes 2. No");
+        int projectChoice = keyboard.nextInt();
+        if (projectChoice == 1) {
+            System.out.println("Would you like to add a task?");
+            System.out.println("1. Yes 2. No");
+            int taskChoice = keyboard.nextInt();
+            if (taskChoice == 1) {
+
+                // assigning task
+                System.out.println("Would you like to assign the task to another memember?");
+                int assignChoice = keyboard.nextInt();
+                System.out.println("1. Yes 2. No");
+                if (assignChoice == 1) {
+
+                } else if (assignChoice == 2) {
+
+                }
+
+                // adding priority to task
+                System.out.println("Would you like to add a priority to the task?");
+                System.out.println("1. Yes 2. No");
+                int priorityChoice = keyboard.nextInt();
+                if (priorityChoice == 1) {
+                    System.out.println(
+                            "Would you like to assign the task as 1. highest priority, 2. medium priority, or 3. low priority");
+                    int priorityRank = keyboard.nextInt();
+                } else if (priorityChoice == 2) {
+
+                }
+
+                // edit task
+                System.out.println("Would you like to edit the task?");
+                int editChoice = keyboard.nextInt();
+                if (editChoice == 1) {
+
+                } else if (editChoice == 2) {
+
+                }
+
+                // add name to ask
+                System.out.println("What would you like to name the task?");
+                String taskName = keyboard.nextLine();
+
+                // add type to task
+                System.out.println("Would you like to add a type for the task?");
+                int typeChoice = keyboard.nextInt();
+                if (typeChoice == 1) {
+
+                } else if (typeChoice == 2) {
+
+                }
+
+                // archive task
+                System.out.println("Would you like to archive the task?");
+                int archiveChoice = keyboard.nextInt();
+                if (archiveChoice == 1) {
+
+                } else if (archiveChoice == 2) {
+
+                }
+
+            } else if (taskChoice == 2) {
+                System.out.println("Would you like to delete the project?");
+                System.out.println("1. Yes 2. No");
+                int deleteChoice = keyboard.nextInt();
+                if (deleteChoice == 1) {
+                    System.out.println("Goodbye");
+                } else if (deleteChoice == 2) {
+
+                }
+            }
+        } else if (projectChoice == 2) {
+            System.out.println("Goodbye");
         } else {
-            System.out.println("Registration failed");
+            System.out.println("Please enter a valid choice");
         }
+
     }
 
     public void scenario() {
