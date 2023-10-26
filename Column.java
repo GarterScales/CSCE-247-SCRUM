ab3e4c8135c487c1183ff594e15df6390e373e1c
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-26 14:21:27 -0400

    working #28

--- Column.java
+++ Column.java
@@ -3,22 +3,36 @@ import java.util.ArrayList;
 public class Column {
     private ArrayList<Task> taskList;
     private String columnName;
+    private LogEnum log;
 
-    public Column(ArrayList<Task> tasks, String name) {
+    public Column(ArrayList<Task> tasks, LogEnum log) {
         this.taskList = tasks;
-        this.columnName = name;
+        this.log = log;
+        setName();
     }
 
     public ArrayList<Task> getTasks() {
         return this.taskList;
     }
 
-    public void setName(String name) {
-        this.columnName = name;
+    public void setName() {
+        if (log.equals(LogEnum.BACKLOG)) {
+            columnName = "Back Log";
+        } else if (log.equals(LogEnum.TODO)) {
+            columnName = "To Do";
+        } else if (log.equals(LogEnum.INPROGRESS)) {
+            columnName = "In Progress";
+        } else if (log.equals(LogEnum.COMPLETE)) {
+            columnName = "Complete";
+        }
     }
 
     public String getName() {
         return columnName;
+    }
+
+    public LogEnum getLog() {
+        return log;
     }
 
     public String toString() {
