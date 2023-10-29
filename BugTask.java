ac440588c84b4cf92f08423fcaab8aadc811cd8e
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-29 16:32:20 -0400

    working #39

--- BugTask.java
+++ BugTask.java
@@ -2,12 +2,34 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class BugTask extends Task {
+    private String reproductionSteps;
+    private String bugEffect;
+
     public BugTask(UUID id, String name, String content, int priority, Log log, int hoursToComplete, UUID userID,
-            ArrayList<Comment> comments, int pointValue) {
+            ArrayList<Comment> comments, int pointValue, String reproductionSteps, String bugEffect) {
         super(id, name, content, priority, log, hoursToComplete, userID, comments, pointValue);
-        // TODO Auto-generated constructor stub
+        setReproductionSteps(reproductionSteps);
+        setBugEffect(bugEffect);
     }
 
-    private ArrayList<String> reproductionSteps;
-    private String bugEffect;
+    public BugTask(String name, String content, int priority, Log log, int hoursToComplete, UUID userID, int pointValue) {
+        super(name, content, priority, log, hoursToComplete, userID, pointValue);
+       
+    }
+
+    public String getReproductionSteps() {
+        return reproductionSteps;
+    }
+
+    public void setReproductionSteps(String newSteps) {
+        reproductionSteps = newSteps;
+    }
+
+    public String getBugEffect() {
+        return bugEffect;
+    }
+
+    public void setBugEffect(String newEffect) {
+        bugEffect = newEffect;
+    }
 }
