bc4d052c88c15fe87ce93f374002b853e790cb5e
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-29 17:13:18 -0400

    working #37, #41, and #42

--- DocumentationTask.java
+++ DocumentationTask.java
@@ -2,11 +2,28 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class DocumentationTask extends Task {
+    private String toDocument;
+    
+    public DocumentationTask(UUID id, String name, String content, int priority, Log log, int hoursToComplete,
+            UUID userID, ArrayList<Comment> comments, int pointValue, String toDocument) {
+        super(id, name, content, priority, log, hoursToComplete, userID, comments, pointValue);
+        setToDocument(toDocument);
+    }
+
     public DocumentationTask(UUID id, String name, String content, int priority, Log log, int hoursToComplete,
             UUID userID, ArrayList<Comment> comments, int pointValue) {
         super(id, name, content, priority, log, hoursToComplete, userID, comments, pointValue);
-        // TODO Auto-generated constructor stub
     }
 
-    private String toDocument;
+    public DocumentationTask(String name, String content, int priority, Log log, int hoursToComplete, UUID userID, int pointValue) {
+        super(name, content, priority, log, hoursToComplete, userID, pointValue);
+    }
+
+    public String getToDocument() {
+        return toDocument;
+    }
+
+    public void setToDocument(String toDocument) {
+        this.toDocument = toDocument;
+    }
 }
