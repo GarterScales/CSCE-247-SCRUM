c84c17f9ee1d1cb6da6bef61e2ec46b5397199a7
Sean Rinkleff <seanrinkleff@gmail.com>
2023-10-31 11:02:49 -0400

    Javadoc for userlist

--- UserList.java
+++ UserList.java
@@ -5,10 +5,18 @@ public class UserList {
     private static UserList instance;
     private ArrayList<User> userList;
 
+    /**
+     * A method that generates a userlist
+     */
     private UserList() {
         userList = DataLoader.getUsers();
     }
 
+    /**
+     * A method that gets an instance of a userlist
+     * 
+     * @return an instance
+     */
     public static UserList getInstance() {
         if (instance == null) {
             instance = new UserList();
@@ -16,10 +24,21 @@ public class UserList {
         return instance;
     }
 
+    /**
+     * A method that gets users for a userlist
+     * 
+     * @return a userlist
+     */
     public ArrayList<User> getUsers() {
         return userList;
     }
 
+    /**
+     * A method that checks if the inputted password meets the requirments
+     * 
+     * @param password
+     * @return
+     */
     public boolean checkPasswordRequirements(String password) {
         char ch;
         boolean hasNumber = false;
@@ -40,6 +59,12 @@ public class UserList {
         return false;
     }
 
+    /**
+     * A method that checks if an inputted Username is available
+     * 
+     * @param username
+     * @return
+     */
     public boolean checkUsernameAvailability(String username) {
         for (User user : userList) {
             if (username.equals(user.getUserName())) {
@@ -49,6 +74,12 @@ public class UserList {
         return true;
     }
 
+    /**
+     * A method that checks if the id ia available
+     * 
+     * @param id
+     * @return
+     */
     public boolean checkIDAvailability(UUID id) {
         for (User user : userList) {
             if (id.equals(user.getId())) {
@@ -58,6 +89,13 @@ public class UserList {
         return true;
     }
 
+    /**
+     * A method that checks if the user exists
+     * 
+     * @param username
+     * @param password
+     * @return
+     */
     public User checkUser(String username, String password) {
         for (User user : userList) {
             if (username.equals(user.getUserName())) {
@@ -71,6 +109,12 @@ public class UserList {
         return null;
     }
 
+    /**
+     * A method that finds the user by the userid
+     * 
+     * @param id
+     * @return
+     */
     public User getUserbyId(UUID id) {
         for (User user : userList) {
             if (id.equals(user.getId())) {
@@ -80,6 +124,15 @@ public class UserList {
         return null;
     }
 
+    /**
+     * A method that adds a user
+     * 
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param username
+     * @param password
+     */
     public void addUser(String firstName, String lastName, String email, String username, String password) {
         userList.add(new User(firstName, lastName, email, username, password));
     }
