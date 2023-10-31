23a135db1887557c922324b2bfaf22991feb9a54
Sean Rinkleff <seanrinkleff@gmail.com>
2023-10-31 11:02:25 -0400

    Javadoc for projectlist

--- ProjectList.java
+++ ProjectList.java
@@ -5,10 +5,18 @@ public class ProjectList {
     public static ProjectList instance;
     private ArrayList<Project> projectList;
 
+    /**
+     * A method that creates a projectlist array
+     */
     private ProjectList() {
         projectList = DataLoader.getProjects();
     }
 
+    /**
+     * A method that returns an instance of a projectlist
+     * 
+     * @return an instance
+     */
     public static ProjectList getInstance() {
         if (instance == null) {
             instance = new ProjectList();
@@ -16,10 +24,20 @@ public class ProjectList {
         return instance;
     }
 
+    /**
+     * Getter for projectlist
+     * 
+     * @return a projectlist
+     */
     public ArrayList<Project> getProjectList() {
         return this.projectList;
     }
 
+    /**
+     * A method that creates a projectboard string
+     * 
+     * @return a returnstring
+     */
     public String ProjectBoard() {
         String returnString = "";
         if (!projectList.isEmpty()) {
@@ -30,11 +48,21 @@ public class ProjectList {
         return returnString;
     }
 
+    /**
+     * A method that adds a project
+     * 
+     * @param name
+     */
     public void addProject(String name) {
         projectList.add(new Project(name));
         DataWriter.saveProjects();
     }
 
+    /**
+     * A method that removes a project
+     * 
+     * @param name
+     */
     public void removeProject(String name) {
         for (int i = 0; i < projectList.size(); i++) {
             if (projectList.get(i).getName().equals(name)) {
@@ -44,6 +72,12 @@ public class ProjectList {
         }
     }
 
+    /**
+     * A method that selects a project
+     * 
+     * @param id
+     * @return
+     */
     public Project selectProject(UUID id) {
         for (Project project : projectList) {
             if (project.getId().equals(id)) {
@@ -54,6 +88,12 @@ public class ProjectList {
         return null;
     }
 
+    /**
+     * A method that checks if an id is availability
+     * 
+     * @param id
+     * @return
+     */
     public boolean checkIDAvailability(UUID id) {
         for (Project project : projectList) {
             if (id.equals(project.getId())) {
